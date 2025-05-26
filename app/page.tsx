4254a98679787d46f2bd10d348d406f2d2b8bcf1
adityarwt1 <adityarawatnew2487@gmail.com>
2025-05-26 22:22:26 +0530

    added link to navigate

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
+
+const LinkArray = [
+    {
+        href: "/counter",
+        title: "Counter Context"
+    }
+]
 
 const page = () => {
   return (
-    <div>
-      home  Page
+    <div className=' flex items-center justify-center w-full h-screen'>
+        <div className=' w-1/2 flex flex-col p-4 shadow-md'>
+        {LinkArray.map((link)=>(
+            <div>
+            <Link href={link.href} className='w-fit   border-gray-600 '>{link.title}</Link>
+            </div>
+        ))}
+        </div>
     </div>
   )
 }
